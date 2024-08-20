dd79068eb2529af13c88935930a7727ad5a52628
zamigtech <zamigtech@gmail.com>
2024-08-20 15:21:29 +0400

    Add GitHub Actions CI pipeline

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class MyViewTests(TestCase):
+    def test_my_view(self):
+        response = self.client.get('/api/v1/resource/?param1=test')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('success', response.json())
--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,20 @@
 from django.http import JsonResponse
 
-
 import logging
 from django.http import JsonResponse
 
 logger = logging.getLogger('my_logger')
 
 
+def response_strategy(data):
+    return {"status": "success", "data": data}
+
+
 def my_view(request):
     params = request.GET
     try:
         logger.info(f"Received request with params: {params}")
-        # Burada bəzi kodlar istisna halı yarada bilər
-        return JsonResponse({"message": "Hello, World!", "params": params})
+        return JsonResponse(response_strategy({"message": "Hello, World!", "params": params}))
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
         return JsonResponse({"message": "An error occurred", "error": str(e)}, status=500)
